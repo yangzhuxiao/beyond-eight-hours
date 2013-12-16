33ab085ecb4f5647b16c53c8bf94efa858e2890f
yangzhuxiao <yangzhuxiao@126.com>
2013-12-16 19:47:55 +0800

    Initial Commit

---
+++ Beyond Eight Hours/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Beyond Eight Hours
+//
+//  Created by xiaozhu on 16/12/13.
+//  Copyright (c) 2013 xiaozhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YXZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YXZAppDelegate class]));
+    }
+}
